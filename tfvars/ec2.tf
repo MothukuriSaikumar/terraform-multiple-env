a49ee4f4cfcf83a0386ecfd4d0deaf9684651763
Saikumar <mskumar.a64@gmail.com>
2025-12-18 23:07:39 +0530

    changes

--- tfvars/ec2.tf
+++ tfvars/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "terraform1" {
-  ami                    = "var.ami_id"
-  instance_type          = "var.instance_type"
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.allow_all.id]
 
 tags = merge(
--- tfvars/variables.tf
+++ tfvars/variables.tf
@@ -23,7 +23,7 @@ variable "environment" {
 }
 variable "instance_type" {
   type = string
-  default = "t2.micro"
+  
 }
 
 variable "cidr_blocks" {
